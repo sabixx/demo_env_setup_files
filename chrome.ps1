bbe5bc7973ba473633f7f457c6c3b37ff124705d
Jens Sabitzer <jens_sabitzer@hotmail.de>
2023-12-17 08:56:47 +0100

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -1,47 +1,37 @@
- # Define Chrome's usual installation paths
-$paths = @(
-    "$env:ProgramFiles\Google\Chrome\Application\chrome.exe", 
-    "$env:ProgramFiles(x86)\Google\Chrome\Application\chrome.exe"
-)
+ # Function to check if Chrome is installed by querying the registry
+function IsChromeInstalled {
+    $path64 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    $path32 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
 
-# Check if Chrome is installed
-$chromeInstalled = $false
-foreach ($path in $paths) {
-    if (Test-Path $path) {
-        $chromeInstalled = $true
-        break
-    }
+    $chrome = Get-ItemProperty $path64, $path32 -ErrorAction SilentlyContinue | 
+              Where-Object { $_.DisplayName -like "*Google Chrome*" }
+
+    return $chrome -ne $null
 }
 
-# Download and Install Chrome if not installed
-  Write-Host "Downloading Google Chrome."
-if (-not $chromeInstalled) {
+# Check if Chrome is installed
+if (-not (IsChromeInstalled)) {
     $chromeInstallerUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
     $installerPath = "$env:TEMP\chrome_installer.exe"
     
+    Write-Host "Downloading and installing Chrome.."
+
     # Download the installer
     Invoke-WebRequest -Uri $chromeInstallerUrl -OutFile $installerPath
 
     # Run the installer
-    Write-Host "Installing Google Chrome..."
     Start-Process -FilePath $installerPath -Args "/silent /install" -Wait
 
     # Clean up the installer
     Remove-Item -Path $installerPath
-}
 
-# Confirm installation
-$chromeInstalled = $false
-foreach ($path in $paths) {
-    if (Test-Path $path) {
-        $chromeInstalled = $true
-        break
+    # Check again after installation attempt
+    if (IsChromeInstalled) {
+        Write-Host "Google Chrome is installed."
+    } else {
+        Write-Host "Failed to install Google Chrome."
     }
-}
-
-if ($chromeInstalled) {
-    Write-Host "Google Chrome is installed."
 } else {
-    Write-Host "Failed to install Google Chrome."
+    Write-Host "Google Chrome is already installed."
 }
  
