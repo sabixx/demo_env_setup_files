ae4d8090f6e1a861e3a24edc64dc3206e322da72
Jens Sabitzer <jens_sabitzer@hotmail.de>
2024-03-09 15:31:32 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,20 +1,29 @@
 
 #Show current state of winrm
+Write-Host "Winrm config:" -ForegroundColor Green
 winrm get winrm/config
 
 #Enable winrm
+Write-Host "winrm quickconfig" -ForegroundColor Green
 winrm quickconfig
 
 # download Python
+Write-Host "download phython" -ForegroundColor Green
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe" -OutFile "$Env:USERPROFILE\Downloads\python-3.9.4-amd64.exe"
 
 # Install python
+Write-Host "install phyton" -ForegroundColor Green
 Start-Process -FilePath "$Env:USERPROFILE\Downloads\python-3.9.4-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+
+# Install pywinrm for ansible
+Write-Host "instal pywinrm" -ForegroundColor Green
+& "C:\Program Files\Python39\Scripts\pip.exe" install pywinrm
 
 # Define the directory for downloading and extracting the zip file
 $downloadDirectory = "C:\install"
 
 # Create the download directory if it doesn't exist
+Write-Host "create download directory" -ForegroundColor Green
 if (-not (Test-Path -Path $downloadDirectory)) {
     New-Item -Path $downloadDirectory -ItemType Directory
 }
@@ -38,16 +47,19 @@ if (-not (Test-Path -Path $downloadDirectory)) {
 }
 
 # Loop through each item in the repository
+Write-Host "Downloading files from github" -ForegroundColor Green
 foreach ($item in $response) {
     $filePath = Join-Path -Path $downloadDirectory -ChildPath $item.name
     DownloadFile $item.download_url $filePath
 }
 
 # Execute the chrome.ps1 script
+Write-Host "Install chrome" -ForegroundColor Green
 $scriptPath = Join-Path -Path $downloadDirectory -ChildPath "chrome.ps1"
 & $scriptPath
 
 # Execute the bginfo.ps1 script
+Write-Host "run BGinfo" -ForegroundColor Green
 $scriptPath = Join-Path -Path $downloadDirectory -ChildPath "bginfo.ps1"
 & $scriptPath
 
